544978668330f529715a1d11bfa6ee2fef23376e
QuocHuannn <truonghuan0709@gmail.com>
2025-10-22 23:41:54 +0700

    chore(utils): remove FocusEffectDemo export from utils index

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,3 @@
 // Focus Effect System Exports
 export * from './focusEffects';
 export { Input, Textarea, Button } from '../components/ui/FocusInput';
-export { FocusEffectDemo } from '../pages/FocusEffectDemo';
